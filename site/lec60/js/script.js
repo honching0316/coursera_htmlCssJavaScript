f20d86787be8b78949f4eb632e1bdbef108a771f
honching <honching@buffalo.edu>
2023-11-10 14:18:53 -0500

    finsihed up lec61 - dynamically display all menu categories

--- site/lec60/js/script.js
+++ site/lec60/js/script.js
@@ -43,6 +43,10 @@ $(
 
     var homeHTML = "snippets/home-snippet.html"
 
+    var allCategoriesUrl = "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
+    var categoriesTitleHtml = "snippets/categories-title-snippet.html";
+    var categoryHtml = "snippets/category-snippet.html";
+
     // Convenience function for inserting innerHTML for the 'selected elem'
     var insertHtml = function (selector, html){
         var targetElem = document.querySelector(selector);
@@ -54,6 +58,17 @@ $(
         var html = "<div class='text-center'>"
         html+= "<img src='images/ajax-loader.gif'></div>";
         insertHtml(selector, html);
+    };
+
+    /*
+    Return substitute of '{{propName}}'
+    with propValue in given 'string'
+    in our case, the string can be category-snippet.html, property Name be short_name, or name, property Value be the actual short name and name of a category
+    */
+    var insertProperty = function (string, propName, propValue){
+        var propToReplace = "{{"+propName+"}}";
+        string = string.replace(new RegExp(propToReplace, "g"), propValue);
+        return string;
     };
 
     // On page load (before images or css)
@@ -71,6 +86,63 @@ $(
             );
         }
     );
+
+    // Load the menu categories view,onclick
+    dc.loadMenuCategories = function(){
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(allCategoriesUrl, buildAndShowCategoriesHTML, true);
+    };
+
+    // Builds HTML for the categories page based on the data
+    // from the server
+    function buildAndShowCategoriesHTML(categories){
+        console.log("categories(string return from firebase server): ", categories);
+        // Load title snippet of categories page
+        $ajaxUtils.sendGetRequest(
+            categoriesTitleHtml,
+            function(categoriesTitleHtml){
+                console.log("categoriesTitleHtml from server response: ", categoriesTitleHtml);
+                // Retrieve single category snippet
+                $ajaxUtils.sendGetRequest(
+                    categoryHtml,
+                    function(categoryHtml){
+                        var categoriesViewHtml = buildCategoriesViewHtml(
+                            categories,
+                            categoriesTitleHtml,
+                            categoryHtml
+                        );
+                        insertHtml("#main-content", categoriesViewHtml);
+                    },
+                    false
+                );
+            },
+            false
+        );
+    }
+
+    // Using categories data and snippets html
+    // build categories view HTML to be inserted into page
+    function buildCategoriesViewHtml(categories, categoriesTitleHtml, categoryHtml){
+        var finalHtml = categoriesTitleHtml;
+        finalHtml += "<section class='row'>";
+        //loop over the categories(array)
+        for(var i=0; i<categories.length; i++){
+            var html=categoryHtml //copy value since primitive type of string
+            var name = ""+categories[i].name;
+            var short_name = categories[i].short_name;
+            // console.log("name: ", name);
+            // console.log("short name: ", short_name);
+            html = insertProperty(html, "name", name);
+            html = insertProperty(html, "short_name", short_name);
+            console.log("html for each category: ", html);
+            finalHtml += html;
+        }
+
+        finalHtml+= "</section>"
+        console.log("final html: ", finalHtml);
+        return finalHtml;
+    }
+
 
     global.$dc = dc;
 
