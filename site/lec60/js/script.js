cdc89fe4b62f15fdf50c13b1d1052e60d7d64397
honching <honching@buffalo.edu>
2023-11-10 17:14:04 -0500

    finished up lec63 - fixing the active button issue

--- site/lec60/js/script.js
+++ site/lec60/js/script.js
@@ -75,6 +75,20 @@ $(
         return string;
     };
 
+    var switchMenuToActive = function (){
+        // Remove 'active' from home button
+        var classes = document.querySelector("#navHomeButton").className;
+        classes = classes.replace(new RegExp("active", "g"), "");
+        document.querySelector("#navHomeButton").className = classes;
+
+        // Add 'active' to menu button if not already there
+        classes = document.querySelector("#navMenuButton").className;
+        if(classes.indexOf("active")==-1){
+            classes += " active";
+            document.querySelector("#navMenuButton").className = classes;
+        }
+    };
+
     // On page load (before images or css)
     document.addEventListener("DOMContentLoaded", 
         function(event){
@@ -121,6 +135,10 @@ $(
                 $ajaxUtils.sendGetRequest(
                     categoryHtml,
                     function(categoryHtml){
+
+                        // Switch CSS class active to mnu button
+                        switchMenuToActive();
+
                         var categoriesViewHtml = buildCategoriesViewHtml(
                             categories,
                             categoriesTitleHtml,
@@ -171,6 +189,8 @@ $(
                 $ajaxUtils.sendGetRequest(
                     menuItemHtml,
                     function (menuItemHtml){
+                        //Switch CSS class active to menu button
+                        switchMenuToActive();
                         var menuItemsViewHtml = buildMenuItemsViewHtml(
                             categoryMenuItems,
                             menuItemsTitleHtml,
